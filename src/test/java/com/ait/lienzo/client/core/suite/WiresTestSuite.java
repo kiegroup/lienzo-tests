b85a89a26b738fb108b1be502f6324063a008e1a
Roger Martínez <roger600@gmail.com>
2018-07-19 00:00:54 +0200

    RHPAM-1406: Stunner - not possible to move Selected nodes in some conditions (#36)
    (cherry picked from commit 0a87a7b11ff3f0e5e3e232d50f745616c4bcf538)

--- src/test/java/com/ait/lienzo/client/core/suite/WiresTestSuite.java
+++ src/test/java/com/ait/lienzo/client/core/suite/WiresTestSuite.java
@@ -24,8 +24,10 @@ import com.ait.lienzo.client.core.shape.wires.WiresManagerTest;
 import com.ait.lienzo.client.core.shape.wires.WiresShapeControlHandleListTest;
 import com.ait.lienzo.client.core.shape.wires.WiresShapeTest;
 import com.ait.lienzo.client.core.shape.wires.handlers.WiresShapeHandlerImplTest;
+import com.ait.lienzo.client.core.shape.wires.handlers.impl.WiresCompositeControlImplTest;
 import com.ait.lienzo.client.core.shape.wires.handlers.impl.WiresCompositeShapeHandlerTest;
 import com.ait.lienzo.client.core.shape.wires.handlers.impl.WiresConnectorControlImplTest;
+import com.ait.lienzo.client.core.shape.wires.handlers.impl.WiresControlPointHandlerImplTest;
 import com.ait.lienzo.client.core.shape.wires.handlers.impl.WiresDockingControlImplTest;
 import com.ait.lienzo.client.core.shape.wires.handlers.impl.WiresParentPickerControlImplTest;
 import com.ait.lienzo.client.core.shape.wires.handlers.impl.WiresShapeControlImplTest;
@@ -52,6 +54,8 @@ import org.junit.runners.Suite;
         WiresConnectorControlImplTest.class,
         WiresShapeHandlerImplTest.class,
         WiresCompositeShapeHandlerTest.class,
+        WiresCompositeControlImplTest.class,
+        WiresControlPointHandlerImplTest.class,
         WiresManagerTest.class,
         MagnetManagerTest.class,
         SelectionManagerTest.class})
